e7d68fc898da49da041e4845df237c1c04f8e1a7
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2023-07-28 17:10:18 +0900

    init

---
+++ create-react-app-5/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import Sample from './Sample';
+
+createRoot(document.getElementById('react-root') as HTMLDivElement).render(<Sample />);
